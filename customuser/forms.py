f25c387ceba6d0ac92404da2436d7788bb748a28
lee-app <asow7345@hallym.ac.kr>
2019-07-17 23:30:56 +0900

    Add custom signup

--- customuser/forms.py
+++ customuser/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserSignUpForm(forms.ModelForm):
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="비밀번호 확인")
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
--- customuser/views.py
+++ customuser/views.py
@@ -14,5 +14,19 @@ from .forms import UserSignUpForm
 from django.views.generic import FormView
 
 class SignUpView(FormView):
-    success_url = '/post/list'
-    template_name = ''
+    success_url = '/accounts/login'
+    form_class = UserSignUpForm
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        password_confirm = form.cleaned_data.get('password_confirm')
+
+        if password == password_confirm:
+            new_user = User.objects.create_user(username=username, email=email, password=password)
+            return super().form_valid(form)
+
+        return super().form_invalid(form)
+        
